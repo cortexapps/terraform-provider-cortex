6ec97f8c5e23a5a39c18e9aa9013e042c416e79d
Shaun McCormick <splittingred@gmail.com>
2023-05-22 13:17:38 -0500

    Docs update

--- examples/resources/resource_definition/resource.tf
+++ examples/resources/resource_definition/resource.tf
@@ -1,6 +1,6 @@
 resource "cortex_resource_definition" "squid-proxy" {
-  type = "squid-proxy"
-  name = "Squid Proxy"
+  type        = "squid-proxy"
+  name        = "Squid Proxy"
   description = "Cortex's customized squid proxy that is used to make requests to firewalled self-managed resources with a static IP."
   schema = {
     properties = {
