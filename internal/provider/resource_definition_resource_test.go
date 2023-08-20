c2650b55aaad371bcb25ad631644e1c3485f6e2b
Shaun McCormick <splittingred@gmail.com>
2023-08-20 10:29:29 -0500

    Improve test coverage

--- internal/provider/resource_definition_resource_test.go
+++ internal/provider/resource_definition_resource_test.go
@@ -23,7 +23,6 @@ func TestAccResourceDefinitionResourceMinimal(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "type", stub.Type),
 					resource.TestCheckResourceAttr(resourceName, "name", stub.Name),
-					resource.TestCheckResourceAttr(resourceName, "description", stub.Description),
 					resource.TestCheckResourceAttr(resourceName, "source", "CUSTOM"),
 				),
 			},
@@ -39,7 +38,6 @@ func TestAccResourceDefinitionResourceMinimal(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "type", stub.Type),
 					resource.TestCheckResourceAttr(resourceName, "name", stub.Name),
-					resource.TestCheckResourceAttr(resourceName, "description", stub.Description),
 					resource.TestCheckResourceAttr(resourceName, "source", "CUSTOM"),
 				),
 			},
