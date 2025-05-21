644cba62a60b025caea27af6f21c02f039264995
Aaron Wirick <aaron.wirick@cortex.io>
2025-05-21 13:05:47 -0700

    Paritally fix up acceptance test for openapi specs
    There's still an outstanding issue here due to some perma drift. The
    cortex API will add on a `servers` block to the yaml file that causes
    state mismatch when a `refresh` is called.

--- internal/provider/catalog_entity_openapi_resource_test.go
+++ internal/provider/catalog_entity_openapi_resource_test.go
@@ -8,11 +8,12 @@ import (
 )
 
 func TestAccCatalogEntityOpenAPIResource(t *testing.T) {
-	entityTag := "test-service"
-	spec := `openapi: 3.0.0
+	entityTag := "manual-test"
+	spec := `
 info:
   title: Test API
   version: 1.0.0
+openapi: 3.0.0
 paths:
   /test:
     get:
@@ -20,33 +21,37 @@ paths:
         '200':
           description: OK`
 
-	updatedSpec := `openapi: 3.0.0
+	updatedSpec := `
 info:
   title: Updated Test API
   version: 1.0.1
+openapi: 3.0.0
 paths:
   /test:
     get:
       responses:
         '200':
           description: OK`
+
+	resourceName := "cortex_catalog_entity_openapi." + entityTag
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
 			// Create and Read testing
+			// TODO:  This is currently failing due to some perma-drift that shows up on refresh.
 			{
 				Config: testAccCatalogEntityOpenAPIResourceConfig(entityTag, spec),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "entity_tag", entityTag),
-					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "spec", spec),
-					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "id", entityTag),
+					resource.TestCheckResourceAttr(resourceName, "entity_tag", entityTag),
+					resource.TestCheckResourceAttr(resourceName, "spec", spec),
+					resource.TestCheckResourceAttr(resourceName, "id", entityTag),
 				),
 			},
 			// ImportState testing
 			{
-				ResourceName:      "cortex_catalog_entity_openapi.test",
+				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
@@ -54,18 +59,17 @@ paths:
 			{
 				Config: testAccCatalogEntityOpenAPIResourceConfig(entityTag, updatedSpec),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "entity_tag", entityTag),
-					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "spec", updatedSpec),
+					resource.TestCheckResourceAttr(resourceName, "entity_tag", entityTag),
+					resource.TestCheckResourceAttr(resourceName, "spec", updatedSpec),
 				),
 			},
-			// Delete testing automatically occurs in TestCase
 		},
 	})
 }
 
 func testAccCatalogEntityOpenAPIResourceConfig(entityTag string, spec string) string {
 	return fmt.Sprintf(`
-resource "cortex_catalog_entity_openapi" "test" {
+resource "cortex_catalog_entity_openapi" %[1]q {
   entity_tag = %[1]q
   spec       = %[2]q
 }
