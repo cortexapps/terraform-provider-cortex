ac110f032e4a92617fb9c07a8f8d89e9a1e51c6e
Your_First_Name Your_Last_Name <youremail@liveoak.bank>
2025-06-10 20:42:57 -0400

    Reference point

--- internal/provider/catalog_entity_openapi_resource_test.go
+++ internal/provider/catalog_entity_openapi_resource_test.go
@@ -5,6 +5,9 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestAccCatalogEntityOpenAPIResource(t *testing.T) {
@@ -36,7 +39,8 @@ paths:
 servers:
     - url: /
 `
-
+	expectedSpec := `{"info":{"title":"Test API","version":"1.0.0"},"paths":{"/test":{"get":{"responses":{"200":{"description":"OK"}}}}},"openapi":"3.0.0","servers":[{"url":"/"}]}`
+	expectedUpdatedSpec := `{"info":{"title":"Updated Test API","version":"1.0.1"},"paths":{"/test":{"get":{"responses":{"200":{"description":"OK"}}}}},"openapi":"3.0.0","servers":[{"url":"/"}]}`
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
@@ -45,6 +49,56 @@ servers:
 			{
 				Config: testAccCatalogEntityOpenAPIResourceConfig(entityTag, spec),
 				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "entity_tag", entityTag),
+					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "spec", expectedSpec),
+					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "id", entityTag),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:      "cortex_catalog_entity_openapi.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			// Update and Read testing
+			{
+				Config: testAccCatalogEntityOpenAPIResourceConfig(entityTag, updatedSpec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "entity_tag", entityTag),
+					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "spec", expectedUpdatedSpec),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func TestAccCatalogEntityOpenAPIResourceJSON(t *testing.T) {
+	entityTag := "test-service"
+	spec := `{"info":{"title":"Test API","version":"1.0.0"},"paths":{"/test":{"get":{"responses":{"200":{"description":"OK"}}}}},"openapi":"3.0.0","servers":[{"url":"/"}]}`
+
+	updatedSpec := `{"info":{"title":"UpdatedTest API","version":"1.0.1"},"paths":{"/test":{"get":{"responses":{"200":{"description":"OK"}}}}},"openapi":"3.0.0","servers":[{"url":"/"}]}`
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccCatalogEntityOpenAPIResourceConfig(entityTag, spec),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.ComposeTestCheckFunc(
+						func(s *terraform.State) error {
+							rs := s.RootModule().Resources["cortex_catalog_entity_openapi.test"]
+							actual := rs.Primary.Attributes["spec"]
+							expected := spec
+
+							if diff := cmp.Diff(expected, actual); diff != "" {
+								t.Errorf("attribute mismatch (-expected +actual):\n%s", diff)
+							}
+
+							return nil
+						},
+					),
 					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "entity_tag", entityTag),
 					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "spec", spec),
 					resource.TestCheckResourceAttr("cortex_catalog_entity_openapi.test", "id", entityTag),
