7e12f9c75ec82ef0c1a007cd38942869b1213baf
Max <zhiyuan.cai@gmail.com>
2025-07-08 14:30:45 -0400

    update tests

--- internal/provider/team_data_source_test.go
+++ internal/provider/team_data_source_test.go
@@ -3,31 +3,59 @@ package provider_test
 import (
 	"testing"
 
+	"github.com/cortexapps/terraform-provider-cortex/internal/cortex"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 )
 
+type TestTeamDataSource struct {
+	Tag           string
+	Members       []cortex.TeamMember
+	SlackChannels []cortex.TeamSlackChannel
+}
+
+func (t *TestTeamDataSource) ToStubTeam() *cortex.Team {
+	return &cortex.Team{
+		TeamTag: t.Tag,
+		CortexTeam: cortex.TeamCortexManaged{
+			Members: t.Members,
+		},
+		SlackChannels: t.SlackChannels,
+	}
+}
+
+func testAccTeamDataSourceConfig(tag string) string {
+	return `
+data "cortex_team" "test-team" {
+  tag = "` + tag + `"
+}
+`
+}
+
 func TestAccTeamDataSource(t *testing.T) {
+	stub := TestTeamDataSource{
+		Tag: "test-team",
+		Members: []cortex.TeamMember{
+			{Name: "max", Email: "mcai@etsy.com"},
+		},
+		SlackChannels: []cortex.TeamSlackChannel{
+			{Name: "test-channel", NotificationsEnabled: true},
+		},
+	}
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Read testing
 			{
-				Config: testAccTeamDataSourceConfig,
+				Config: testAccTeamDataSourceConfig(stub.Tag),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.cortex_team.engineering", "tag", "platform_engineering"),
-					resource.TestCheckResourceAttrSet("data.cortex_team.engineering", "members.0.name"),
-					resource.TestCheckResourceAttrSet("data.cortex_team.engineering", "members.0.email"),
-					resource.TestCheckResourceAttrSet("data.cortex_team.engineering", "slack_channels.0.name"),
-					resource.TestCheckResourceAttrSet("data.cortex_team.engineering", "slack_channels.0.notifications_enabled"),
+					resource.TestCheckResourceAttr("data.cortex_team.test-team", "tag", stub.Tag),
+					resource.TestCheckResourceAttr("data.cortex_team.test-team", "members.0.name", stub.Members[0].Name),
+					resource.TestCheckResourceAttr("data.cortex_team.test-team", "members.0.email", stub.Members[0].Email),
+					resource.TestCheckResourceAttr("data.cortex_team.test-team", "slack_channels.0.name", stub.SlackChannels[0].Name),
+					resource.TestCheckResourceAttr("data.cortex_team.test-team", "slack_channels.0.notifications_enabled", "true"),
 				),
 			},
 		},
 	})
 }
-
-const testAccTeamDataSourceConfig = `
-data "cortex_team" "engineering" {
-  tag = "platform_engineering"
-}
-`
