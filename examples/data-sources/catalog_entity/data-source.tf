a9be39d17a7e3b6da9f993fa89f94473996bc013
Tom Whiting <tom.whiting@kraken.tech>
2026-04-07 11:38:30 +0100

    Update docs with changes to the catalog entity data sources

--- examples/data-sources/catalog_entity/data-source.tf
+++ examples/data-sources/catalog_entity/data-source.tf
@@ -1,3 +1,19 @@
 data "cortex_catalog_entity" "products-service" {
   tag = "products-service"
 }
+
+output "products_service_name" {
+  value = data.cortex_catalog_entity.products-service.name
+}
+
+output "products_service_type" {
+  value = data.cortex_catalog_entity.products-service.type
+}
+
+output "products_service_groups" {
+  value = data.cortex_catalog_entity.products-service.groups
+}
+
+output "products_service_owners" {
+  value = data.cortex_catalog_entity.products-service.ownership
+}
